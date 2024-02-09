035eb55ce81405f4bdbb8aedbfa3e5993ea1828a
Rishabh Thakur <155800024+Rishabh-prime@users.noreply.github.com>
2024-02-10 03:17:59 +0530

    Add files via upload

---
+++ anime.js
@@ -0,0 +1,15 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems,Options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+  
+
+  
